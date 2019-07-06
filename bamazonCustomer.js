464689d51d89d23e7a00d2116c1d4bbef7d08559
steliosk9 <steliosk9@gmail.com>
2019-07-06 10:00:16 -0500

    npm cli-table3 & display function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,24 +1,87 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table3');
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "",
     database: "bamazon_db"
   });
-  
+ 
+  connection.connect();
 
-  connection.connect(function(err) {
+   var display = function() {
+  connection.query("SELECT * FROM products",function(err,res){
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+    // console.log("connected as id " + connection.threadId);
+    console.log("");
+    console.log("========================");
+    console.log("Welcome to Bamazon");
+    console.log("========================");
+    console.log("");
+ 
   
+  // instantiate
+  var table = new Table({
+      head: ['Product id', 'Product Description','stock','Price']
+    , colWidths: [8, 30 ,20, 8]
+  });
+   
+  // table is an Array, so you can `push`, `unshift`, `splice` and friends
+  for(var i = 0; i < res.length; i++) {
+  table.push([res[i].id, res[i].name, res[i].stock_quantity, res[i].price]);
+  }
+
+  console.log(table.toString());
+  console.log("");
+     startPrompt();
+});
+
+};
+   
+// display();
+
+
+function startPrompt() {
+
+  inquirer.prompt([{
+
+    type: "input",
+    name: "product",
+    message: "please add the id of the product you are interesting to purchase"
+  }])
+ .then(function(response){
+   var choise = response.product;
+   connection.query("SELECT * FROM products WHERE id =?",choise ,function(err,res){
+     if (err)throw err;
+     if (res.length === 0){
+       console.log("Product doesnt exist,please enter a valid choise");
+
+       startPrompt();
+
+     }else{
+      inquirer.prompt({
+        type:"Input",
+        name:"quantity",
+        message:"How many items would you like to purchase?"
+      })
+      .then(function(responseTwo){
+        var number = responseTwo.quantity;
+        if(number > res[0].stock_quantity){
+          console.log("oops!!!...Unfortunately we only have "+ res[0].stock_quantity +"Items from the selected product");
+          startPrompt();
+          } else {
+          console.log("");
+          console.log(res[0].name + " purchased");
+          console.log("price " +"$"+ res[0].price);
+          }
+      })
+     }
+   })
+  })
+} 
+
+display();
+
